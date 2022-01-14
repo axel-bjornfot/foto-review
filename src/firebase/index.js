0ccd093610f00eaaa0dfb80d13561cfccbaaa417
Axel Björnfot <axelbjornfot@gmail.com>
2022-01-14 16:59:56 +0100

    fuck my keys

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-	apiKey: process.env.VITE_FIREBASE_API_KEY,
-	authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.VITE_FIREBASE_APP_ID,
+	apiKey: "AIzaSyBldNdhBLRxv7IasPclPiLULSkhvxwlaOw",
+	authDomain: "foto-review-7769d.firebaseapp.com",
+	projectId: "foto-review-7769d",
+	storageBucket: "foto-review-7769d.appspot.com",
+	messagingSenderId: "989751294719",
+	appId: "1:989751294719:web:f32df87ef9c3e23f2beaca",
 };
 
 const app = initializeApp(firebaseConfig);
